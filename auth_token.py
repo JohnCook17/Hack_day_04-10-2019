a43aee584a8045749690af1ed8b5be9613c66abf
Amber <abmcbride5@gmail.com>
2019-10-04 19:13:47 +0000

    made into a method

--- auth_token.py
+++ auth_token.py
@@ -4,13 +4,12 @@
 import sys
 import requests
 
-if __name__ == "__main__":
-    ''' 
-    needs to eventually be turned to a method but this prints and returns the auth_token
-    when the user enters parameters api_key, email, password, in that order
-    '''
-    authentication = requests.post("https://intranet.hbtn.io/users/auth_token.json", params={'api_key': sys.argv[1], 'email': sys.argv[2], 'password': sys.argv[3], 'scope': 'checker' })
-    print(sys.argv[0], sys.argv[1], sys.argv[2], sys.argv[3])
+def get_auth(api_key, email, password):
+    authentication = requests.post("https://intranet.hbtn.io/users/auth_token.json", 
+    params={'api_key': api_key, 'email': email, 'password': password, 'scope': 'checker' })
+
     auth_dict = authentication.json()
+    
     print(auth_dict.get('auth_token'))
+    
     return auth_dict.get('auth_token')
