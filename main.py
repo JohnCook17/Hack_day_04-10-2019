6a6b0ee03f5fe652b8837b9bf9ff8f8aa39e86f6
JohnCook17 <655@holbertonschool.com>
2019-10-04 10:56:31 -0700

    added documentation

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python3
 
-#importing os
+# importing os
 import os
-#import gitpython
+# import gitpython
 try:
     import git
 except ImportError:
     print("please install gitpython, use sudo pip3 install gitpython")
     print("see https://gitpython.readthedocs.io/en/stable/intro.html for more details")
 
+# get checker api key and check if they are missing any checks
+# if missing checks clone repo to check for basic mistakes
+
+# path and repo below, should probably move to json file
+
 path = "/mnt/c/dev/test_repo"
 repo = "https://github.com/JohnCook17/holbertonschool-zero_day.git"
 
+
+# make path and git clone the repo to the path
 try:
     os.mkdir(path)
 except OSError:
@@ -20,3 +27,4 @@ else:
     print("Successfully created the directory %s" % path)
 
 git.Git(path).clone(repo)
+
