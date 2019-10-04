1f171d44b4d2afaa8b982dbbbf2eb5a924bd231a
JohnCook17 <655@holbertonschool.com>
2019-10-04 16:36:48 -0700

    pushing work so others can take a look, all files return false on exe

--- checksOnChecks/check_README.py
+++ checksOnChecks/check_README.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 if __name__ != "__main__":
-
+    
+    import os
+    import sys
+    import json
     import json
     from os import path
+
+
     class Checks():
         def check_README(self, my_path):
             """ checks if the readme file is at least 100 char long 
@@ -10,16 +15,51 @@ if __name__ != "__main__":
             with open("vars.json") as vars:
                 variable = json.load(vars)
                 repo = variable["repo"].split("/")[4].split(".")[0]
+                subfolder = variable["subfolder"]
                 print(repo)
-            readme_path = my_path + "/" + repo + "/README.md"
-            if path.exists(readme_path):
-                with open(my_path + "/" + repo + "/README.md") as r:
-                    if len(r.read()) < 100:
-                        print("README.md does not exist or is not long enough.")
-                        return 0
-                    else:
-                        print("README.md looks good, but can always be better.")
-                        return 1
+                print(subfolder)
+            if not subfolder:
+                readme_path = my_path + "/" + repo + "/README.md"
             else:
-                print("README.md is missing")
-                return 0
+                readme_path = my_path + "/" + repo + "/" + subfolder + "/README.md"
+                if path.exists(readme_path):
+                    print(readme_path)
+                    with open(readme_path) as r:
+                        readme = r.read()
+                        print(readme)
+                        if len(readme) < 100:
+                            print("README.md is not long enough.")
+                            return 0
+                        else:
+                            print("README.md looks good, but can always be better.")
+                            return 1
+                else:
+                    print("README.md is missing")
+                    return 0
+
+
+        def check_executable(self, my_path):
+            """ Tested with dummy files  but needs to be implemented with 
+            filepath but checks for exec permissions"""
+            with open("vars.json") as vars:
+                variable = json.load(vars)
+                repo = variable["repo"].split("/")[4].split(".")[0]
+                subfolder = variable["subfolder"]
+                print(repo)
+                print(subfolder)
+            if not subfolder:
+                file_path = my_path + "/" + repo
+            else:
+                file_path = my_path + "/" + repo + "/" + subfolder
+            for dir_name, subdirlist, filelist in os.walk(file_path):
+                print("working in %s" % dir_name)
+                for file_name in filelist:
+                    print(filelist)
+                    print("=============================")
+                    print(file_name)
+                    try:
+                        path_ok = os.access(file_name, os.X_OK)
+                    except IOError as err:
+                        print(err)
+                    print("Check if %s file is executable:"% file_name, path_ok)
+
