48ae3efb37b72badc56aef77f3dc69e39a046289
Ken-Mens <kenneth.mensah@gmail.com>
2019-10-04 23:24:02 +0000

    commented out for loop of get checks

--- checksOnChecks/get_checks.py
+++ checksOnChecks/get_checks.py
@@ -5,7 +5,7 @@ auth = __import__('auth_token').get_auth
 
 
 if __name__ == "__main__":
-    auth_id = auth('f08b491db334657dc706e958b807c2c7', '682@holbertonschool.com', '3zPk&fkG6mAf')
+    auth_id = auth('593323cf3f7cfcb59ed1725f340eab58', '525@holbertonschool.com', 'Mbappe107')
     def find_failed_checks():
         """ Method that gets all the tasks with failed checks"""
         tasks = {};
@@ -18,13 +18,19 @@ if __name__ == "__main__":
         for taskname, taskid in tasks.items():
             req_correction = requests.post('https://intranet.hbtn.io/tasks/{}/start_correction.json?auth_token={}'.format(taskid, auth_id))
             correction_id = (req_correction.json().get('id'))
-            correction_res = requests.get('https://intranet.hbtn.io/correction_requests/{}.json?auth_token={}'.format(correction_id, auth_id))
+            print(correction_id)
+        """    correction_res = requests.get('https://intranet.hbtn.io/correction_requests/{}.json?auth_token={}'.format(correction_id, auth_id))
             check_list = correction_res.json().get('result_display').get('checks')
+            print(check_list)
             for check in check_list:
+                print(check)	
                 if check.get('check_label') == "requirement" and check.get('passed') is False:
+                    print("It's going in there")
                     failed_tasks['taskname'] = taskid
-
+       
+ 		
         print(failed_tasks)
         return failed_tasks
+       """
 
 find_failed_checks()
