0020b5163e4498c7ac8ec53f766656017e8f5413
Amber <abmcbride5@gmail.com>
2019-10-05 04:44:57 +0000

    pushing working copy

--- checksOnChecks/get_checks.py
+++ checksOnChecks/get_checks.py
@@ -8,21 +8,26 @@ task_info = {}
 def find_failed_checks(auth_id, proj_num, correction_ids=[]):
     """ Method that gets all the tasks with failed checks"""
     failed_tasks = {}
+    list_of_dicts = []
     if not correction_ids:
-        get_correction_ids(auth_id, correction_ids, task_info, proj_num)
+        correction_ids = get_correction_ids(auth_id, correction_ids, task_info, proj_num)
         while(1):
             for correction_id in correction_ids:
                 correction_res = requests.get('https://intranet.hbtn.io/correction_requests/{}.json?auth_token={}'.format(correction_id, auth_id))
                 check_list = correction_res.json().get('result_display').get('checks')
                 print("Working")
                 print("Still Working")
-                for check in check_list:
-                    if check.get('check_label') == "requirement" and check.get('passed') == False:
-                        failed_tasks[check.get('title')] = check.get('passed')
+
+                if check_list is not []:
+                    for check in check_list:
+                        if check.get('check_label') == "requirement" and check.get('passed') is False:
+                            failed_tasks[check.get('title')] = check.get('passed')
+            if failed_tasks:
+                list_of_dicts.append(failed_tasks)
+            failed_tasks = {}
             if correction_res.json().get('status') == 'Done':
                 break
-        print(failed_tasks)
-        return failed_tasks
+    return list_of_dicts
 
 
 def get_correction_ids(auth_id, correction_ids, task_info, proj_num):
@@ -36,4 +41,4 @@ def get_correction_ids(auth_id, correction_ids, task_info, proj_num):
         correction_id = (req_correction.json().get('id'))
         correction_ids.append(correction_id)
         task_info[taskname] = taskid
-
+    return correction_ids
